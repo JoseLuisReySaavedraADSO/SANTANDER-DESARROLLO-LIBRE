8949862fb77b51fa266ec56e4c2a495deb4ac5e7
dg.wrq109@gmail.com <dg.wrq109@gmail.com>
2023-09-14 17:03:32 -0500

    refactor: :bug: Avances views}

--- resources/views/layouts/searchDog.blade.php
+++ resources/views/layouts/searchDog.blade.php
@@ -80,28 +80,8 @@
 
                 <section class="container">
         
-                    <div class="changeBackground">
-                        <input class="input" type="radio" id="changeImage">
-                        <input class="input" type="radio" id="changeImage">
-                    </div>
-        
-                    <div class="title">
-                        <h1 class="title__main">Tu mejor amigo <span class="title__second">en casa!</span></h1>
-                    </div>
-        
-                    <p class="paragraph">Adoptar la mascota perfecta de más de 700 refugios de animales...</p>
-        
-                    <div class="picture" id="changeImage">
-                        <img class="picture__image" src="{{asset('images/slide1.png')}}" alt="">
-                    </div>
-        
-                    <!-- <div class="picture picture2" id="changeImage">
-                        <img class="picture__image" src="images/slide2.png" alt="">
-                    </div> -->
-        
-                    <a class="link" href="#"><img src="{{asset('images/chevron-right.svg')}}" alt=""></a>
-        
-        
+                    
+
                 </section>
                     
             </main>
