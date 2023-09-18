ccb65e02855197296a2549fda497c503c161e633
JoseLuisReySaavedraADSO <reysaavedra123@gmail.com>
2023-09-18 14:57:17 -0500

    style: :art: Se agregan vistas mejoradas y css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,58 +1,41 @@
-@extends('layouts.app')
+@extends('layouts.back')
 
 @section('content')
+<main class="login">
+    <form class="form" method="POST" action="{{ route('login') }}">
 
-<main class="login__container">
-    
-    <section class="login">
+            <input placeholder="Correo Electrónico" id="email" type="text" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+            @error('email')
+                <span class="form__error invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
 
-        <div class="title">
-            <h1 class="title__main">Tu mejor amigo <span class="title__second">en casa!</span></h1>
-        </div>
+        @csrf
 
-        <form class="form" method="POST" action="{{ route('login') }}">
-            @csrf
+            <input placeholder="Contraseña" id="password" type="password" class="@error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
-            <div class="form__inputContainer">
-                <input placeholder="Correo Electrónico" id="email" type="email" class="form__input form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+            @error('password')
+                <span class="form__error invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        
+        {{-- <input type="password" name="clave" placeholder="Contraseña"> --}}
 
-                @error('email')
-                    <span class="form__error invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-
-            <div class="form__inputContainer">
-                <input placeholder="Contrseña" id="password" type="password" class="form__input form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                @error('password')
-                    <span class="form__error invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-
-            <div class="form__inputContainer">
-                <button type="submit" class="form__input form__button btn btn-primary">
-                    {{ __('Login') }}
-                </button>
-
-                {{-- @if (Route::has('password.request'))
-                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                        {{ __('Forgot Your Password?') }}
-                    </a>
-                @endif --}}
-            </div>
+        
+            <button type="submit" class="form__input form__button btn btn-primary">
+            {{ __('Login') }}
+            </button>
 
             <div class="form__inputContainer">
                 <p class="form__inputContainer--text">¿Aún no posee una cuenta? <a class="form__inputContainer--link" href="{{ route('register') }}">Regístrate</a> </p>
             </div>
+            
+            {{-- <button type="submit">Ingresar</button>
+        
+            <p class="form__inputContainer--text">¿Aún no posee una cuenta? <a class="form__inputContainer--link" href="{{ route('register') }}">Regístrate</a> </p> --}}
 
-        </form>
-
-    </section>
-
+    </form>
 </main>
-
 @endsection
