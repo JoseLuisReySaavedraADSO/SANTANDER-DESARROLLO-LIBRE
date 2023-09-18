a28a6f09a16e474c98816a1e2429e084711bda63
dg.wrq109@gmail.com <dg.wrq109@gmail.com>
2023-09-18 16:23:17 -0500

    Intento fallido vistas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,37 +4,38 @@
 <main class="login">
     <form class="form" method="POST" action="{{ route('login') }}">
 
-            <input placeholder="Correo Electrónico" id="email" type="text" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-            @error('email')
-                <span class="form__error invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
+        <input placeholder="Correo Electrónico" id="email" type="text" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+        @error('email')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
 
         @csrf
 
-            <input placeholder="Contraseña" id="password" type="password" class="@error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+        <input placeholder="Contraseña" id="password" type="password" class="@error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
-            @error('password')
-                <span class="form__error invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        
-        {{-- <input type="password" name="clave" placeholder="Contraseña"> --}}
+        @error('password')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    
+    {{-- <input type="password" name="clave" placeholder="Contraseña"> --}}
 
-        
-            <button type="submit" class="form__input form__button btn btn-primary">
-            {{ __('Login') }}
-            </button>
+    
+        <button type="submit" class="btn btn-primary">
+        {{ __('Login') }}
+        </button>
 
-            <div class="form__inputContainer">
-                <p class="form__inputContainer--text">¿Aún no posee una cuenta? <a class="form__inputContainer--link" href="{{route('register')}}">Regístrate</a> </p>
-            </div>
-            
-            {{-- <button type="submit">Ingresar</button>
+        <div class="form__inputContainer">
+            <p class="form__inputContainer--text">¿Aún no posee una cuenta? <a class="form__inputContainer--link" href="{{route('register')}}">Regístrate</a> </p>
+        </div>
         
-            <p class="form__inputContainer--text">¿Aún no posee una cuenta? <a class="form__inputContainer--link" href="{{ route('register') }}">Regístrate</a> </p> --}}
+        
+        {{-- <button type="submit">Ingresar</button>
+    
+        <p class="form__inputContainer--text">¿Aún no posee una cuenta? <a class="form__inputContainer--link" href="{{ route('register') }}">Regístrate</a> </p> --}}
 
     </form>
 </main>
